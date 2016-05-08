6ee1b6dea33f4b7af4c00199beae3c6d549a1b1b
Mark <mark.chiavaroli@iag.com.au>
2016-05-09 00:42:48 +1000

    Test CI failure

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,7 +4,7 @@ describe 'main' do
   describe 'landing page' do
     it 'returns Hello Grusshable World' do
       get '/'
-      expect(last_response.body).to eq 'Hello Grusshable World'
+      expect(last_response.body).to eq 'Hello World'
     end
   end
 end
