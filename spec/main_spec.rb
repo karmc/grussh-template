e8f6fc903040fb7703b902142442c1947bb1430f
Mark <mark.chiavaroli@iag.com.au>
2016-05-09 11:38:49 +1000

    Changed landing page output

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'main' do
   describe 'landing page' do
-    it 'returns Hello Grusshable World' do
+    it 'returns Hello Peters World' do
       get '/'
-      expect(last_response.body).to eq 'Hello Grusshable World'
+      expect(last_response.body).to eq 'Hello Peters World'
     end
   end
 end
